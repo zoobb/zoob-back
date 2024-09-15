f5cd288b67cbd4c27acb06a2883c432ef2fe82e2
zoob <zubovalexey2001@gmail.com>
2024-09-15 22:02:59 +0300

    implement add to list req handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"math/rand"
@@ -9,13 +10,20 @@ import (
 	"time"
 )
 
+type AddToListReqBody struct {
+	ListItem string `json:"listItem"`
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
+	var todoList []string
 	port := 8247
-	http.HandleFunc("/", handler)
+
+	http.HandleFunc("/", handlePingReq)
+	http.HandleFunc("/list", handleAddToListReq(todoList))
 	// I KNOW
 	log.Println("Server started on port " + strconv.Itoa(port))
 	err := http.ListenAndServe("localhost:"+strconv.Itoa(port), nil)
@@ -24,7 +32,7 @@ func main() {
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handlePingReq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -48,6 +56,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Random number sent:", randomInt)
 	if err != nil {
 		return
+	}
+}
+
+func handleAddToListReq(l []string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		if r.Method == http.MethodPost {
+			var reqBody AddToListReqBody
+			err := json.NewDecoder(r.Body).Decode(&reqBody)
+			if err != nil {
+				http.Error(w, "Invalid request body", http.StatusBadRequest)
+				return
+			}
+			log.Println("Request body:", reqBody)
+
+			l = append(l, reqBody.ListItem)
+			log.Println(l)
+			w.WriteHeader(http.StatusOK)
+		}
 	}
 }
 
